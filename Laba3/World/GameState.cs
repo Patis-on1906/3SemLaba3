ffbd171bcbd3a735c292c93c9db1a054742fd63e
Админ <@DESKTOP-M0CQGDO>
2025-12-05 10:19:42 +0500

    add CellType, Map

--- Laba3/World/GameState.cs
+++ Laba3/World/GameState.cs
@@ -3,7 +3,20 @@ namespace Laba3;
 public class GameState
 {
     public Player Player { get; set; }
-    public Map Map { get; set; }
+    public Map CurrentMap { get; set; }
     public List<Enemy> Enemies { get; set; } = new();
     public List<Treasure> Treasures { get; set; } = new();
+    public string GameVersion { get; set; }
+    public DateTime SaveTime { get; set; }
+
+    public GameState(Map map, string gameVersion = "1.0")
+    {
+
+        CurrentMap = map ?? throw new ArgumentNullException(nameof(map));
+        GameVersion = gameVersion;
+        SaveTime = DateTime.Now;
+    }
+
+    public GameState() { }
+
 }
